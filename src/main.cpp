61131f2ad2dbb4ef0c036ad31325360c6b1a2d64
sleepingsaint <suryasantosh14523@gmail.com>
2022-10-11 22:39:37 +0530

    base setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#define GLFW_INCLUDE_VULKAN
+#define VMA_IMPLEMENTATION
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <VkBootstrap.h>
+#include <vk_mem_alloc.h>
+
+int main(){
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
